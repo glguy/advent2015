83eda99738b6086c3c5d4741b9f1667ddd340bd6
Eric Mertens <emertens@gmail.com>
2015-12-10 09:47:10 -0800

    Put starting value in its own input file

--- Day10.hs
+++ Day10.hs
@@ -4,17 +4,19 @@ import Data.List
 
 main :: IO ()
 main =
-  do let step40 = iterate step key !! 40
+  do step0 <- loadInput
+
+     let step40 = iterate lookAndSay step0 !! 40
      print (length step40)
 
-     let step50 = iterate step step40 !! 10
+     let step50 = iterate lookAndSay step40 !! 10
      print (length step50)
 
-key :: String
-key = "1113222113"
+loadInput :: IO String
+loadInput = head . words <$> readFile "input10.txt"
 
-step :: String -> String
-step = foldr aux [] . group
+lookAndSay :: String -> String
+lookAndSay = foldr aux [] . group
   where
   aux xs = shows (length xs)
          . showChar (head xs)
