2d7f81f0f1632da66c5efc869e739ef1599c33a9
Eric Mertens <emertens@gmail.com>
2015-12-09 21:07:59 -0800

    Day10

---
+++ Day10.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Data.List
+
+main :: IO ()
+main =
+  do let step40 = iterate step key !! 40
+     print (length step40)
+
+     let step50 = iterate step step40 !! 10
+     print (length step50)
+
+key :: String
+key = "1113222113"
+
+step :: String -> String
+step x = aux =<< group x
+  where
+  aux xs = shows (length xs) [head xs]
