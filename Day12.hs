f58d05b2dcd956ae88a61d66a8d73529de9b96ab
Eric Mertens <emertens@gmail.com>
2015-12-13 13:08:42 -0800

    Clean up and document parts of Day12

--- Day12.hs
+++ Day12.hs
@@ -10,18 +10,23 @@ main :: IO ()
 main =
   do input <- loadInput
      print (sumOfNumbers input)
+     print (sumOfNonredNumbers input)
 
-     let pruned = transform trim input
-     print (sumOfNumbers pruned)
-
+-- | Sum of all numbers in a JSON value.
 sumOfNumbers :: Value -> Scientific
 sumOfNumbers = sumOf (deep _Number)
 
-trim :: Value -> Value
-trim v
-  | elemOf (_Object . folded . _String) "red" v = _Number # 0
-  | otherwise = v
+-- | Sum of all numbers in a JSON value after
+-- pruning out portions that fail the 'noRed' test.
+sumOfNonredNumbers :: Value -> Scientific
+sumOfNonredNumbers = sumOf (deepOf (plate . filtered noRed) _Number)
 
+-- | Returns True for all Values that are not Objects
+-- with any @"red"@ values.
+noRed :: Value -> Bool
+noRed = notElemOf (_Object . folded) "red"
+
+-- | Load the input file as a JSON value.
 loadInput :: IO Value
 loadInput = (^?! _Value) <$> readFile "input12.txt"
 
